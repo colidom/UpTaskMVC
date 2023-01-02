43181f6e14bff879f2ae423a1d89f682025d22d0
colidom <colidom@outlook.com>
2023-01-02 00:44:42 +0000

    Primeros Pasos con el Routing

---
+++ controllers/LoginController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Controllers;
+
+
+class LoginController
+{
+    public static function login()
+    {
+        echo "Desde login";
+
+        if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+        }
+    }
+
+    public static function logout()
+    {
+        echo "Desde login";
+    }
+
+    public static function crear()
+    {
+        echo "Desde crear";
+    }
+
+    public static function olvide()
+    {
+        echo "Desde olvide";
+
+        if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+        }
+    }
+
+    public static function restablecer()
+    {
+        echo "Desde restablecer";
+
+        if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+        }
+    }
+
+    public static function mensaje()
+    {
+        echo "Desde mensaje";
+    }
+
+    public static function confirmar()
+    {
+        echo "Desde confirmar";
+    }
+}

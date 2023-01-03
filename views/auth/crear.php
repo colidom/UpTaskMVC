9cefe5f82dde44f72b0bd1e909c0f54eb25895a7
colidom <colidom@outlook.com>
2023-01-03 22:26:17 +0000

    Creando la Vista de Crear Cuenta

--- views/auth/crear.php
+++ views/auth/crear.php
@@ -1,1 +1,34 @@
-<h1>Crear cuenta</h1>
+<div class="contenedor crear">
+    <?php include_once __DIR__ . '/../templates/nombre-sitio.php'; ?>
+    <div class="contenedor-sm">
+        <p class="descripcion-pagina">Crear cuenta en UpTask</p>
+
+        <form method="POST" action="/" class="formulario">
+            <div class="campo">
+                <label for="nombre">Nombre</label>
+                <input type="text" id="nombre" placeholder="Tu nombre" name="nombre">
+            </div>
+
+            <div class="campo">
+                <label for="email">Email</label>
+                <input type="email" id="email" placeholder="Tu email" name="email">
+            </div>
+
+            <div class="campo">
+                <label for="password">Password</label>
+                <input type="password" id="password" placeholder="Tu password" name="password">
+            </div>
+
+            <div class="campo">
+                <label for="password2">Repetir Password</label>
+                <input type="password" id="password2" placeholder="Repetir tu password" name="password2">
+            </div>
+
+            <input type="submit" class="boton" value="Iniciar sesión">
+        </form>
+        <div class="acciones">
+            <a href="/">¿Ya tienes una cuenta? Iniciar sesión</a>
+            <a href="/olvide">¿Olvidaste tu password? Recuperar</a>
+        </div>
+    </div><!-- .contenedor-sm -->
+</div>

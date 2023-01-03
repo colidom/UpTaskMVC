b9a7418e96d35c3bbe4a54cdfef4fddf8b1c9f24
colidom <colidom@outlook.com>
2023-01-03 00:14:27 +0000

    Creando el Formulario de Login

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,1 +1,26 @@
-<h1>Desde Login</h1>
+<div class="contenedor">
+    <h1>UpTask</h1>
+    <p>Crea y administra tus proyectos</p>
+
+    <div class="contenedor-sm">
+        <p class="descripcion-pagina">Iniciar sesión</p>
+
+        <form method="POST" action="/" class="formulario">
+            <div class="campo">
+                <label for="email">Email</label>
+                <input type="email" id="email" placeholder="Tu email" name="email">
+            </div>
+
+            <div class="campo">
+                <label for="password">Password</label>
+                <input type="password" id="password" placeholder="Tu password" name="password">
+            </div>
+
+            <input type="submit" class="boton" value="Iniciar sesión">
+        </form>
+        <div class="acciones">
+            <a href="/crear">¿Aún no tienes una cuenta? Crear</a>
+            <a href="/olvide">¿Olvidaste tu password? Recuperar</a>
+        </div>
+    </div><!-- .contenedor-sm -->
+</div>

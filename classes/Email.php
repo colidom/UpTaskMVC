53fcb02033f25526870b05468a346be169a273a3
colidom <colidom@outlook.com>
2023-01-05 01:05:11 +0000

    Enviando el Email de Confirmación

--- classes/Email.php
+++ classes/Email.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace Classes;
+namespace classes;
+
+use PHPMailer\PHPMailer\PHPMailer;
 
 class Email
 {
@@ -17,5 +19,33 @@ class Email
 
     public function enviarConfirmacion()
     {
+        // Crear el objeto de mail
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Host = 'smtp.mailtrap.io';
+        $mail->SMTPAuth = true;
+        $mail->Port = 2525;
+        $mail->Username = '06ed95c3a9b8b4';
+        $mail->Password = 'faeb551dc75a24';
+
+        $mail->setFrom("cuentas@uptask.com");
+        $mail->addAddress("cuentas@uptask.com", "uptask.com");
+        $mail->Subject = "Confirma tu cuenta";
+
+        // Set HTML
+        $mail->isHTML(true);
+        $mail->CharSet = "UTF-8";
+
+        $contenido = "<html>";
+        $contenido .= "<p><strong>Hola " . $this->nombre . ",</br>";
+        $contenido .= "</strong> Has creado tu cuenta en UpTask, solo debes confirmarla presionando en el siguiente enlace</p>";
+        $contenido .= "<p>Presiona aquí: <a href='http://localhost:3000/confirmar?token=" . $this->token . "'>Confirmar cuenta</a></p>";
+        $contenido .= "<p>Si no has creado esta cuenta, puedes ignorar este correo</p>";
+        $contenido .= "</html>";
+
+        $mail->Body = $contenido;
+
+        // Enviar el email
+        $mail->send();
     }
 }

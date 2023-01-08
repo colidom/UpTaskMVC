7bb1c6de92952c7dbad5c6462257f6ad72e4a8bf
colidom <colidom@outlook.com>
2023-01-08 22:04:39 +0000

    Arreglada ruta

--- views/templates/sidebar.php
+++ views/templates/sidebar.php
@@ -3,7 +3,7 @@
 
     <nav class="sidebar-nav">
         <a class="<?php echo ($titulo === 'Proyectos') ? 'activo' : ''; ?>" href="/dashboard">Proyectos</a>
-        <a class="<?php echo ($titulo === 'Crear proyecto') ? 'activo' : ''; ?>" href="/crar-proyecto">Crear Proyecto</a>
+        <a class="<?php echo ($titulo === 'Crear proyecto') ? 'activo' : ''; ?>" href="/crear-proyecto">Crear Proyecto</a>
         <a class="<?php echo ($titulo === 'Perfil') ? 'activo' : ''; ?>" href="/perfil">Perfil</a>
     </nav>
 </aside>

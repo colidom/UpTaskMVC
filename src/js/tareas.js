7406810ac88f1a0c8a05e3a3320971f52b85460c
colidom <colidom@outlook.com>
2023-01-15 22:45:49 +0000

    Creando el Modal para añadir Tareas

--- src/js/tareas.js
+++ src/js/tareas.js
@@ -4,5 +4,23 @@
   const nuevaTareaBtn = document.querySelector("#agregar-tarea");
   nuevaTareaBtn.addEventListener("click", mostrarFormulario);
 
-  function mostrarFormulario() {}
+  function mostrarFormulario() {
+    const modal = document.createElement("DIV");
+    modal.classList.add("modal");
+    modal.innerHTML = `
+        <form class="formulario nueva-tarea">
+            <legend>Añade una nueva tarea</legend>
+            <div class="campo">
+                <label>Tarea</label>
+                <input type="text" name="tarea" placeholder="Añadir tarea al proyecto actual"
+                id="tarea"/>
+            </div>
+            <div class="opciones">
+                <input type="submit" class="submit-nueva-tarea" name="tarea" value="Añadir tarea"/>
+            <button type="button" class="cerrar-modal">Cancelar</button>
+            </div>
+        </form>
+    `;
+    document.querySelector("body").appendChild(modal);
+  }
 })();

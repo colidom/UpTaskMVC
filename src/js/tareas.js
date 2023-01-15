29ca5084de477d3956097c00d00672593ef2fa56
colidom <colidom@outlook.com>
2023-01-15 22:37:09 +0000

    Añadiendo JavaScript para mostrar el Formulario

---
+++ src/js/tareas.js
@@ -0,0 +1,8 @@
+// IIFE -> Inmediatelly Invoque Function Expression
+(function () {
+  // Botón para mostrar ventana modal para agregar tarea
+  const nuevaTareaBtn = document.querySelector("#agregar-tarea");
+  nuevaTareaBtn.addEventListener("click", mostrarFormulario);
+
+  function mostrarFormulario() {}
+})();

ef224314616f9ff7ac0c29092e17035e532745f5
colidom <colidom@outlook.com>
2023-01-15 22:55:47 +0000

    Aplicando CSS al Modal

--- src/js/tareas.js
+++ src/js/tareas.js
@@ -21,6 +21,10 @@
             </div>
         </form>
     `;
+    setTimeout(() => {
+      const formulario = document.querySelector(".formulario");
+      formulario.classList.add("animar");
+    }, 3000);
     document.querySelector("body").appendChild(modal);
   }
 })();

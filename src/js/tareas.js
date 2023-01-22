2562a4fd9bffdd262f20d724cd52d2fd7644ff43
colidom <colidom@outlook.com>
2023-01-22 18:54:19 +0000

    Construyendo y enviando la petición al Servidor

--- src/js/tareas.js
+++ src/js/tareas.js
@@ -197,7 +197,31 @@
         actualizarTarea(tarea);
     }
 
-    function actualizarTarea(tarea) {}
+    async function actualizarTarea(tarea) {
+        const { estado, id, nombre, proyectoId } = tarea;
+        const datos = new FormData();
+        datos.append('id', id),
+            datos.append('nombre', nombre),
+            datos.append('estado', estado),
+            datos.append('proyectoId', obtenerProyecto());
+
+        // Debuguear FormData
+        /* for (let valor of datos.values()) {
+            console.log(valor);
+        } */
+
+        try {
+            const url = 'http://localhost:3000/api/tarea/actualizar';
+            const respuesta = await fetch(url, {
+                method: 'POST',
+                body: datos,
+            });
+            const resultado = await respuesta.json();
+            console.log(respuesta);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     function obtenerProyecto() {
         const proyectoParams = new URLSearchParams(window.location.search);
